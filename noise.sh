3c47c9ab1f3a5fa0e3bd3b9c67ed96a91ba4e9d7
youichiro <cinnamon416@gmail.com>
2019-08-25 15:59:18 +0900

    fix

--- noise.sh
+++ noise.sh
@@ -3,14 +3,15 @@ set -e
 set -x
 
 mkdir $DATA_ART
+CORPUS=$DATA/art_corpus.tgt
 
 # generate data with noise
-python noise_data.py -e 1 -s 9182
-python noise_data.py -e 2 -s 78834
-python noise_data.py -e 3 -s 5101
-python noise_data.py -e 4 -s 33302
-python noise_data.py -e 5 -s 781
-python noise_data.py -e 6 -s 1092
-python noise_data.py -e 7 -s 10688
-python noise_data.py -e 8 -s 50245
-python noise_data.py -e 9 -s 71187
+python noise_data.py -c $CORPUS -o $DATA_ART -e 1 -s 9182
+python noise_data.py -c $CORPUS -o $DATA_ART -e 2 -s 78834
+python noise_data.py -c $CORPUS -o $DATA_ART -e 3 -s 5101
+python noise_data.py -c $CORPUS -o $DATA_ART -e 4 -s 33302
+python noise_data.py -c $CORPUS -o $DATA_ART -e 5 -s 781
+python noise_data.py -c $CORPUS -o $DATA_ART -e 6 -s 1092
+python noise_data.py -c $CORPUS -o $DATA_ART -e 7 -s 10688
+python noise_data.py -c $CORPUS -o $DATA_ART -e 8 -s 50245
+python noise_data.py -c $CORPUS -o $DATA_ART -e 9 -s 71187
