19f0bae0965947dee28d9d50ee0514836b3f801d
youichiro <cinnamon416@gmail.com>
2020-01-28 17:11:22 +0900

    fix

--- gec_model.sh
+++ gec_model.sh
@@ -2,29 +2,33 @@
 set -e
 set -u
 
-if [ $# -ne 2 ]; then
-  echo "usage: python generate.sh <device> <exp>" 1>&2
+if [ $# -ne 3 ]; then
+  echo "usage: python generate.sh <device> <exp> <result>" 1>&2
   exit 1
 fi
 
-device=$1
-exp=$2
+DEVICE=$1
+EXP=$2
+RESULT=$3
 
-DATA_RAW='naist_clean_char'
+# TEST_PREF='naist_clean_char'
+TEST_PREF='lang8_char.dev'
 
 EPOCH='_last'
 MODEL_PATH="out/models/models${EXP}/checkpoint${EPOCH}.pt"
 OPTION_FILE='option_files/exp.txt'
-DATA_RAW="out/data_raw/${DATA_RAW}"
-TEST_DATA="data/${DATA_RAW}.src"
-SAVE_DIR="out/results/result${EXP}/${DATA_RAW}"
-SAVE_FILE="output_gecmodel${EPOCH}.char.txt"
-OUTPUT_M2_FILE="m2score_gecmodel${EPOCH}.char.txt"
+TEST_DATA="data/${TEST_PREF}.src"
+DATA_RAW="out/data_raw/naist_clean_char"
+SAVE_DIR="out/results/result${EXP}/${TEST_PREF}/${RESULT}"
+SAVE_FILE="output${EPOCH}.char.txt"
+OUTPUT_M2_FILE="m2score${EPOCH}.char.txt"
 KENLM_DATA='/lab/ogawa/tools/kenlm/data/bccwj_clean2_char/bccwj_clean2_char.4gram.binary'
 KENLM_WEIGHT=0.5
 
+mkdir -p $SAVE_DIR
 
-CUDA_VISIBLE_DEVICES=$device python gec_model.py \
+
+CUDA_VISIBLE_DEVICES=$DEVICE python gec_model.py \
   --model-path $MODEL_PATH \
   --data-raw $DATA_RAW \
   --option-file $OPTION_FILE \
@@ -36,3 +40,5 @@ CUDA_VISIBLE_DEVICES=$device python gec_model.py \
 
 python2 ./gec_scripts/m2scorer/m2scorer -v ${SAVE_DIR}/${SAVE_FILE} data/${DATA_RAW}.m2 > ${SAVE_DIR}/${OUTPUT_M2_FILE}
 tail -n 3 ${SAVE_DIR}/${OUTPUT_M2_FILE}
+
+python /lab/ogawa/scripts/slack/send_slack_message.py -m "[${HOSTNAME}] Finish gec_model"
