77179c919edec6db55c74c298061f61565222300
youichiro <cinnamon416@gmail.com>
2020-01-28 18:22:50 +0900

    fix

--- gec_model.sh
+++ gec_model.sh
@@ -22,10 +22,15 @@ DATA_RAW="out/data_raw/naist_clean_char"
 SAVE_DIR="out/results/result${EXP}/${TEST_PREF}/${RESULT}"
 SAVE_FILE="output${EPOCH}.char.txt"
 OUTPUT_M2_FILE="m2score${EPOCH}.char.txt"
-KENLM_DATA='/lab/ogawa/tools/kenlm/data/bccwj_clean2_char/bccwj_clean2_char.4gram.binary'
-KENLM_WEIGHT=0.0
+KENLM_DATA='/lab/ogawa/tools/kenlm/data/bccwj_clean2_char/bccwj_clean2_char.6gram.binary'
+KENLM_WEIGHT=0.5
 
 mkdir -p $SAVE_DIR
+
+if [[ -f ${SAVE_DIR}/${SAVE_FILE} ]]; then
+  echo "already exists: ${SAVE_DIR}/${SAVE_FILE}" 1>&2
+  exit 1
+fi
 
 
 CUDA_VISIBLE_DEVICES=$DEVICE python gec_model.py \
@@ -42,4 +47,4 @@ echo "| calc M2score"
 python2 ./gec_scripts/m2scorer/m2scorer -v ${SAVE_DIR}/${SAVE_FILE} data/${TEST_PREF}.m2 > ${SAVE_DIR}/${OUTPUT_M2_FILE}
 tail -n 3 ${SAVE_DIR}/${OUTPUT_M2_FILE}
 
-python /lab/ogawa/scripts/slack/send_slack_message.py -m "[${HOSTNAME}] Finish gec_model"
+python /lab/ogawa/scripts/slack/send_slack_message.py -m "[${HOSTNAME}] Finish gec_model: ${RESULT}"
