17e142a02b56df1c049cd0570ad367282281fdd1
youichiro <cinnamon416@gmail.com>
2020-02-01 02:50:55 +0900

    fix

--- gec_model.sh
+++ gec_model.sh
@@ -11,8 +11,8 @@ DEVICE=$1
 EXP=$2
 RESULT=$3
 
-TEST_PREF='naist_clean_char'
-# TEST_PREF='lang8_char.dev'
+# TEST_PREF='naist_clean_char'
+TEST_PREF='lang8_char.dev'
 
 EPOCH='_last'
 MODEL_PATH="out/models/models${EXP}/checkpoint${EPOCH}.pt"
@@ -22,9 +22,11 @@ DATA_RAW="out/data_raw/naist_clean_char"
 SAVE_DIR="out/results/result${EXP}/${TEST_PREF}/${RESULT}"
 SAVE_FILE="output${EPOCH}.char.txt"
 OUTPUT_M2_FILE="m2score${EPOCH}.char.txt"
+TRANSLM_DATA='out/models_lm/bccwj_clean2_char+lang8_char_train/checkpoint_last.pt'
+TRANSLM_DICT='out/data_bin_lm/bccwj_clean2_char+lang8_char_train/dict.txt'
 KENLM_DATA='/lab/ogawa/tools/kenlm/data/bccwj_clean2_char/bccwj_clean2_char.6gram.binary'
-KENLM_WEIGHT=0.0
-N_ROUND=6
+LM_WEIGHT=0.05
+N_ROUND=1
 
 mkdir -p $SAVE_DIR
 
@@ -41,12 +43,14 @@ CUDA_VISIBLE_DEVICES=$DEVICE python gec_model.py \
   --test-data $TEST_DATA \
   --save-dir $SAVE_DIR \
   --save-file $SAVE_FILE \
-  --lm kenlm \
-  --lm-data $KENLM_DATA \
-  --lm-weight $KENLM_WEIGHT \
+  --lm transformer_lm \
+  --lm-data $LM_DATA \
+  --lm-dict $TRANSLM_DICT \
+  --lm-weight $LM_WEIGHT \
   --n-round $N_ROUND
 
 echo "| calc M2score"
 python2 ./gec_scripts/m2scorer/m2scorer -v ${SAVE_DIR}/${SAVE_FILE} data/${TEST_PREF}.m2 > ${SAVE_DIR}/${OUTPUT_M2_FILE}
 tail -n 3 ${SAVE_DIR}/${OUTPUT_M2_FILE}
 
+python /lab/ogawa/scripts/slack/send_slack_message.py -m "Finish gec_model: $RESULT ($HOSTNAME)"
